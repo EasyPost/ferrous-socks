757479aab46b24b03e7788bb00876900831ac81f
James Brown <jbrown@easypost.com>
2020-02-26 20:11:43 -0800

    add tests

--- src/acl.rs
+++ src/acl.rs
@@ -52,15 +52,66 @@ impl Acl {
                 .map(|i| i.contains(ip))
                 .unwrap_or(true);
             let port_match = rule.destination_port.map(|p| p == dport).unwrap_or(true);
-            let username_match = if let Some(u) = username {
-                rule.username.as_ref().map(|p| p == u).unwrap_or(true)
-            } else {
-                false
+            let username_match = match (username, rule.username.as_ref()) {
+                (Some(found), Some(expected)) => (found == expected),
+                (Some(_found), None) => true,
+                (None, Some(_)) => false,
+                (None, None) => true,
             };
             if ip_match && port_match && username_match {
                 return rule.action.permitted();
             }
         }
         self.default_action.permitted()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{Acl, AclAction, AclItem};
+    use ip_network::IpNetwork;
+    use std::net::IpAddr;
+
+    #[test]
+    fn test_basic() {
+        let acl = Acl::from_parts(
+            vec![
+                AclItem {
+                    action: AclAction::Reject,
+                    destination_network: None,
+                    destination_port: None,
+                    username: Some("evil".to_owned()),
+                },
+                AclItem {
+                    action: AclAction::Allow,
+                    destination_network: None,
+                    destination_port: Some(80),
+                    username: None,
+                },
+                AclItem {
+                    action: AclAction::Allow,
+                    destination_network: Some(
+                        IpNetwork::new("127.0.0.0".parse::<IpAddr>().unwrap(), 8).unwrap(),
+                    ),
+                    destination_port: None,
+                    username: None,
+                },
+                AclItem {
+                    action: AclAction::Allow,
+                    destination_network: None,
+                    destination_port: None,
+                    username: Some("backdoor".to_owned()),
+                },
+            ],
+            AclAction::Reject,
+        );
+        let localhost = "127.0.0.1".parse::<IpAddr>().unwrap();
+        let non_localhost = "1.1.1.1".parse::<IpAddr>().unwrap();
+        assert_eq!(acl.is_permitted(None, localhost, 22), true);
+        assert_eq!(acl.is_permitted(None, non_localhost, 22), false);
+        assert_eq!(acl.is_permitted(None, non_localhost, 80), true);
+        assert_eq!(acl.is_permitted(Some("nobody"), non_localhost, 80), true);
+        assert_eq!(acl.is_permitted(Some("evil"), non_localhost, 80), false);
+        assert_eq!(acl.is_permitted(Some("backdoor"), non_localhost, 22), true);
     }
 }
